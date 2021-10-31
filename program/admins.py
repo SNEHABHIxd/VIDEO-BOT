88bba2a3af879d17dd2560124bc417af7daf4768
SNEHABHIxd <93216312+SNEHABHIxd@users.noreply.github.com>
2021-10-31 22:44:56 +0530

    Update admins.py

--- program/admins.py
+++ program/admins.py
@@ -9,7 +9,7 @@ from config import BOT_USERNAME, GROUP_SUPPORT, IMG_3, UPDATES_CHANNEL
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 
-@Client.on_message(command(["reload", f"reload@{BOT_USERNAME}"]) & other_filters)
+@Client.on_message(command(["reload", f"reload@SNEHABHI_VIDEOBOT"]) & other_filters)
 @authorized_users_only
 async def update_admin(client, message):
     global admins
@@ -19,11 +19,11 @@ async def update_admin(client, message):
         new_admins.append(u.user.id)
     admins[message.chat.id] = new_admins
     await message.reply_text(
-        "✅ Bot **reloaded correctly !**\n✅ **Admin list** has been **updated !**"
+        "✅ 𝚂𝙽𝙴𝙷𝙰𝙱𝙷𝙸 𝚅𝙸𝙳𝙴𝙾 𝙿𝙻𝙰𝚈𝙴𝚁 **𝚁𝙴𝙻𝙾𝙰𝙳𝙴𝙳 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙻𝚈 !**\n✅ **𝙰𝙳𝙼𝙸𝙽 𝙻𝙸𝚂𝚃** 𝙷𝙰𝚂 𝙱𝙴𝙴𝙽 **𝚄𝙿𝙳𝙰𝚃𝙴S 𝙱𝚈 @SNEHABHI_UPDATES !**"
     )
 
 
-@Client.on_message(command(["skip", f"skip@{BOT_USERNAME}", "vskip"]) & other_filters)
+@Client.on_message(command(["skip", f"skip@SNEHABHI_VIDEOBOT", "vskip"]) & other_filters)
 @authorized_users_only
 async def skip(client, m: Message):
 
@@ -31,10 +31,10 @@ async def skip(client, m: Message):
         [
             [
                 InlineKeyboardButton(
-                    text="✨ ɢʀᴏᴜᴘ", url=f"https://t.me/{GROUP_SUPPORT}"
+                    text="✨ ɢʀᴏᴜᴘ", url=f"https://t.me/SNEHABHI_SERVER"
                 ),
                 InlineKeyboardButton(
-                    text="🌻 ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/{UPDATES_CHANNEL}"
+                    text="🌻 ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/SNEHABHI_UPDATES"
                 ),
             ]
         ]
@@ -44,13 +44,13 @@ async def skip(client, m: Message):
     if len(m.command) < 2:
         op = await skip_current_song(chat_id)
         if op == 0:
-            await m.reply("❌ nothing is currently playing")
+            await m.reply("❌ 𝙽𝙾𝚃𝙷𝙸𝙽𝙶 𝙸𝚂 𝙲𝚄𝚁𝚁𝙴𝙽𝚃𝙻𝚈 𝙿𝙻𝙰𝚈𝙸𝙽𝙶")
         elif op == 1:
-            await m.reply("✅ __Queues__ is empty.\n\n• userbot leaving voice chat")
+            await m.reply("✅ __𝚀𝚄𝙴𝚄𝙴𝚂__ 𝙸𝚂 𝙴𝙼𝙿𝚃𝚈.\n\n• 𝚂𝙽𝙴𝙷𝙰𝙱𝙷𝙸 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 𝙻𝙴𝙰𝚅𝙸𝙽𝙶 𝚅𝙲")
         else:
             await m.reply_photo(
                 photo=f"{IMG_3}",
-                caption=f"⏭ **Skipped to the next track.**\n\n🏷 **Name:** [{op[0]}]({op[1]})\n💭 **Chat:** `{chat_id}`\n💡 **Status:** `Playing`\n🎧 **Request by:** {m.from_user.mention()}",
+                caption=f"⏭ **𝚂𝙺𝙸𝙿𝙿𝙴𝙳 𝚃𝙾 𝚃𝙷𝙴 𝙽𝙴𝚇𝚃 𝚃𝚁𝙰𝙲𝙺.**\n\n🏷 **𝙽𝙰𝙼𝙴:** [{op[0]}]({op[1]})\n💭 **𝙲𝙷𝙰𝚃:** `{chat_id}`\n💡 **𝚂𝚃𝙰𝚃𝚄𝚂:** `𝙿𝙻𝙰𝚈𝙸𝙽𝙶`\n🎧 **𝚁𝙴𝚀𝚄𝙴𝚂𝚃 𝙱𝚈:** {m.from_user.mention()}",
                 reply_markup=keyboard,
             )
     else:
@@ -72,7 +72,7 @@ async def skip(client, m: Message):
 
 
 @Client.on_message(
-    command(["stop", f"stop@{BOT_USERNAME}", "end", f"end@{BOT_USERNAME}", "vstop"])
+    command(["stop", f"stop@SNEHABHI_VIDEOBOT", "end", f"end@SNEHABHI_VIDEOBOT", "vstop"])
     & other_filters
 )
 @authorized_users_only
@@ -82,15 +82,15 @@ async def stop(client, m: Message):
         try:
             await call_py.leave_group_call(chat_id)
             clear_queue(chat_id)
-            await m.reply("✅ **streaming has ended.**")
+            await m.reply("✅ **𝚂𝚃𝚁𝙴𝙰𝙼𝙸𝙽𝙶 𝙷𝙰𝚂 𝙴𝙽𝙳𝙴𝙳 𝚄𝙿𝙻𝙾𝙰𝙳 𝙱𝚈 @SNEHABHI_UPDATES.**")
         except Exception as e:
-            await m.reply(f"🚫 **error:**\n\n`{e}`")
+            await m.reply(f"🚫 **𝙴𝚁𝚁𝙾𝚁:**\n\n`{e}`")
     else:
-        await m.reply("❌ **nothing in streaming**")
+        await m.reply("❌ **𝙽𝙾𝚃𝙷𝙸𝙽𝙶 𝙸𝙽 𝚂𝚃𝚁𝙴𝙰𝙼𝙸𝙽𝙶 𝚄𝙿𝙻𝙾𝙰𝙳 𝙱𝚈 @SNEHABHI_UPDATES**")
 
 
 @Client.on_message(
-    command(["pause", f"pause@{BOT_USERNAME}", "vpause"]) & other_filters
+    command(["pause", f"pause@SNEHABHI_VIDEOBOT", "vpause"]) & other_filters
 )
 @authorized_users_only
 async def pause(client, m: Message):
@@ -99,16 +99,16 @@ async def pause(client, m: Message):
         try:
             await call_py.pause_stream(chat_id)
             await m.reply(
-                "⏸ **Track paused.**\n\n• **To resume the stream, use the**\n» /resume command."
+                "⏸ **𝚃𝚁𝙰𝙲𝙺 𝙿𝙰𝚄𝚂𝙴.**\n\n• **𝚃𝙾 𝚁𝙴𝚂𝚄𝙼𝙴 𝚃𝙷𝙴 𝚂𝚃𝚁𝙴𝙰𝙼 , 𝚄𝚂𝙴 𝚃𝙷𝙴**\n» /resume 𝙲𝙾𝙼𝙼𝙰𝙽𝙳."
             )
         except Exception as e:
-            await m.reply(f"🚫 **error:**\n\n`{e}`")
+            await m.reply(f"🚫 **𝙴𝚁𝚁𝙾𝚁:**\n\n`{e}`")
     else:
-        await m.reply("❌ **nothing in streaming**")
+        await m.reply("❌ **𝙽𝙾𝚃𝙷𝙸𝙽𝙶 𝙸𝙽 𝚂𝚃𝚁𝙴𝙰𝙼𝙸𝙽𝙶 𝚄𝙿𝙻𝙾𝙰𝙳 𝙱𝚈 @SNEHABHI_UPDATES**")
 
 
 @Client.on_message(
-    command(["resume", f"resume@{BOT_USERNAME}", "vresume"]) & other_filters
+    command(["resume", f"resume@SNEHABHI_VIDEOBOT", "vresume"]) & other_filters
 )
 @authorized_users_only
 async def resume(client, m: Message):
@@ -117,16 +117,16 @@ async def resume(client, m: Message):
         try:
             await call_py.resume_stream(chat_id)
             await m.reply(
-                "▶️ **Track resumed.**\n\n• **To pause the stream, use the**\n» /pause command."
+                "▶️ **𝚃𝚁𝙰𝙲𝙺 𝚁𝙴𝚂𝚄𝙼𝙴.**\n\n• **𝚃𝙾 𝙿𝙰𝚄𝚂𝙴 𝚃𝙷𝙴 𝚂𝚃𝚁𝙴𝙰𝙼 , 𝚄𝚂𝙴 𝚃𝙷𝙴**\n» /pause 𝙲𝙾𝙼𝙼𝙰𝙽𝙳."
             )
         except Exception as e:
-            await m.reply(f"🚫 **error:**\n\n`{e}`")
+            await m.reply(f"🚫 **𝙴𝚁𝚁𝙾𝚁:**\n\n`{e}`")
     else:
-        await m.reply("❌ **nothing in streaming**")
+        await m.reply("❌ **𝙽𝙾𝚃𝙷𝙸𝙽𝙶 𝙸𝙽 𝚂𝚃𝚁𝙴𝙰𝙼𝙸𝙽𝙶 𝚄𝙿𝙻𝙾𝙰𝙳 𝙱𝚈 @SNEHABHI_UPDATES**")
 
 
 @Client.on_message(
-    command(["mute", f"mute@{BOT_USERNAME}", "vmute"]) & other_filters
+    command(["mute", f"mute@SNEHABHI_VIDEOBOT", "vmute"]) & other_filters
 )
 @authorized_users_only
 async def mute(client, m: Message):
@@ -135,16 +135,16 @@ async def mute(client, m: Message):
         try:
             await call_py.mute_stream(chat_id)
             await m.reply(
-                "🔇 **Userbot muted.**\n\n• **To unmute the userbot, use the**\n» /unmute command."
+                "🔇 **𝚂𝙽𝙴𝙷𝙰𝙱𝙷𝙸 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 𝙼𝚄𝚃𝙴𝙳.**\n\n• **𝚃𝙾 𝚄𝙽𝙼𝚄𝚃𝙴 𝚂𝙽𝙴𝙷𝙰𝙱𝙷𝙸 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 , 𝚄𝚂𝙴 𝚃𝙷𝙴**\n» /unmute 𝙲𝙾𝙼𝙼𝙰𝙽𝙳."
             )
         except Exception as e:
-            await m.reply(f"🚫 **error:**\n\n`{e}`")
+            await m.reply(f"🚫 **𝙴𝚁𝚁𝙾𝚁:**\n\n`{e}`")
     else:
-        await m.reply("❌ **nothing in streaming**")
+        await m.reply("❌ **𝙽𝙾𝚃𝙷𝙸𝙽𝙶 𝙸𝙽 𝚂𝚃𝚁𝙴𝙰𝙼𝙸𝙽𝙶 𝚄𝙿𝙻𝙾𝙰𝙳 𝙱𝚈 @SNEHABHI_UPDATES**")
 
 
 @Client.on_message(
-    command(["unmute", f"unmute@{BOT_USERNAME}", "vunmute"]) & other_filters
+    command(["unmute", f"unmute@SNEHABHI_VIDEOBOT", "vunmute"]) & other_filters
 )
 @authorized_users_only
 async def unmute(client, m: Message):
@@ -153,16 +153,16 @@ async def unmute(client, m: Message):
         try:
             await call_py.unmute_stream(chat_id)
             await m.reply(
-                "🔊 **Userbot unmuted.**\n\n• **To mute the userbot, use the**\n» /mute command."
+                "🔊 **𝚂𝙽𝙴𝙷𝙰𝙱𝙷𝙸 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 𝚄𝙽𝙼𝚄𝚃𝙴D.**\n\n• **𝚃𝙾 𝙼𝚄𝚃𝙴𝙳 𝚂𝙽𝙴𝙷𝙰𝙱𝙷𝙸 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 , 𝚄𝚂𝙴 𝚃𝙷𝙴**\n» /mute 𝙲𝙾𝙼𝙼𝙰𝙽𝙳."
             )
         except Exception as e:
-            await m.reply(f"🚫 **error:**\n\n`{e}`")
+            await m.reply(f"🚫 **𝙴𝚁𝚁𝙾𝚁:**\n\n`{e}`")
     else:
-        await m.reply("❌ **nothing in streaming**")
+        await m.reply("❌ **𝙽𝙾𝚃𝙷𝙸𝙽𝙶 𝙸𝙽 𝚂𝚃𝚁𝙴𝙰𝙼𝙸𝙽𝙶 𝚄𝙿𝙻𝙾𝙰𝙳 𝙱𝚈 @SNEHABHI_UPDATES**")
 
 
 @Client.on_message(
-    command(["volume", f"volume@{BOT_USERNAME}", "vol"]) & other_filters
+    command(["volume", f"volume@SNEHABHI_VIDEOBOT", "vol"]) & other_filters
 )
 @authorized_users_only
 async def change_volume(client, m: Message):
@@ -170,6 +170,6 @@ async def change_volume(client, m: Message):
     chat_id = m.chat.id
     try:
         await call_py.change_volume_call(chat_id, volume=int(range))
-        await m.reply(f"✅ **volume set to** `{range}`%")
+        await m.reply(f"✅ **𝚅𝙾𝙻𝚄𝙼𝙴 𝚂𝙴𝚃 𝚃𝙾** `{range}`%")
     except Exception as e:
-        await m.reply(f"🚫 **error:**\n\n{e}")
+        await m.reply(f"🚫 **𝙴𝚁𝚁𝙾𝚁:**\n\n{e}")
