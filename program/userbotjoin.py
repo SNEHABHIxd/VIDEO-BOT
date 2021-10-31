2d7d85ab7c8b76f3f8dd1f9efdc6ee8dace55448
SNEHABHIxd <93216312+SNEHABHIxd@users.noreply.github.com>
2021-10-31 22:02:40 +0530

    Update userbotjoin.py

--- program/userbotjoin.py
+++ program/userbotjoin.py
@@ -8,7 +8,7 @@ from pyrogram.errors import UserAlreadyParticipant
 
 
 @Client.on_message(
-    command(["userbotjoin", f"userbotjoin@{BOT_USERNAME}"]) & ~filters.private & ~filters.bot
+    command(["userbotjoin", f"userbotjoin@SNEHABHI_VIDEOBOT"]) & ~filters.private & ~filters.bot
 )
 @authorized_users_only
 @errors
@@ -18,7 +18,7 @@ async def join_group(client, message):
         invitelink = await client.export_chat_invite_link(chid)
     except BaseException:
         await message.reply_text(
-            "• **i'm not have permission:**\n\n» ❌ __Add Users__",
+            "• **𝙸'𝙼 𝙽𝙾𝚃 𝙷𝙰𝚅𝙴 𝙿𝙴𝚁𝙼𝙸𝚂𝚂𝙸𝙾𝙽:**\n\n» ❌ __𝙰𝙳𝙳 𝚄𝚂𝙴𝚁𝚂__",
         )
         return
 
@@ -39,26 +39,26 @@ async def join_group(client, message):
         )
         return
     await message.reply_text(
-        f"✅ **userbot succesfully entered chat**",
+        f"✅ **𝚂𝙽𝙴𝙷𝙰𝙱𝙷𝙸 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 𝚂𝚄𝙲𝙲𝙴𝚂𝚂𝙵𝚄𝙻𝙻𝚈 𝙹𝙾𝙸𝙽𝙴𝙳 𝚃𝙷𝙴 𝙶𝚁𝙾𝚄𝙿**",
     )
 
 
 @Client.on_message(command(["userbotleave",
-                            f"leave@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
+                            f"leave@SNEHABHI_VIDEOBOT"]) & filters.group & ~filters.edited)
 @authorized_users_only
 async def leave_one(client, message):
     try:
-        await USER.send_message(message.chat.id, "✅ userbot successfully left chat")
+        await USER.send_message(message.chat.id, "✅ 𝚂𝙽𝙴𝙷𝙰𝙱𝙷𝙸 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 𝚂𝚄𝙲𝙲𝙴𝚂𝚂𝙵𝚄𝙻𝙻𝚈 𝙻𝙴𝙵𝚃 𝚃𝙷𝙴 𝙶𝚁𝙾𝚄𝙿")
         await USER.leave_chat(message.chat.id)
     except BaseException:
         await message.reply_text(
-            "❌ **userbot couldn't leave your group, may be floodwaits.**\n\n**» or manually kick userbot from your group**"
+            "❌ **𝚂𝙽𝙴𝙷𝙰𝙱𝙷𝙸 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 𝙲𝙾𝚄𝙻𝙳𝙽'𝚃 𝙻𝙴𝙰𝚅𝙴 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿, 𝙼𝙰𝚈 𝙱𝙴 𝙵𝙻𝙾𝙾𝙳𝚆𝙰𝙸𝚃𝚂.**\n\n**» 𝙾𝚁 𝙼𝙰𝙽𝚄𝙰𝙻𝙻𝚈 𝙺𝙸𝙲𝙺 𝚂𝙽𝙴𝙷𝙰𝙱𝙷𝙸 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 𝙵𝚁𝙾𝙼 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿**"
         )
 
         return
 
 
-@Client.on_message(command(["leaveall", f"leaveall@{BOT_USERNAME}"]))
+@Client.on_message(command(["leaveall", f"leaveall@SNEHABHI_VIDEOBOT"]))
 @sudo_users_only
 async def leave_all(client, message):
     if message.from_user.id not in SUDO_USERS:
@@ -66,20 +66,20 @@ async def leave_all(client, message):
 
     left = 0
     failed = 0
-    lol = await message.reply("🔄 **userbot** leaving all chats !")
+    lol = await message.reply("🔄 **𝚂𝙽𝙴𝙷𝙰𝙱𝙷𝙸 𝚄𝚂𝙴𝚁𝙱𝙾𝚃** 𝙻𝙴𝙰𝚅𝙸𝙽𝙶 𝙰𝙻𝙻 𝙶𝚁𝙾𝚄𝙿𝚂 !")
     async for dialog in USER.iter_dialogs():
         try:
             await USER.leave_chat(dialog.chat.id)
             left += 1
             await lol.edit(
-                f"Userbot leaving all group...\n\nLeft: {left} chats.\nFailed: {failed} chats."
+                f"𝚂𝙽𝙴𝙷𝙰𝙱𝙷𝙸 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 𝙻𝙴𝙰𝚅𝙸𝙽𝙶 𝙰𝙻𝙻 𝙶𝚁𝙾𝚄𝙿𝚂...\n\n𝙻𝙴𝙵𝚃: {left} 𝙲𝙷𝙰𝚃𝚂.\n𝙵𝙰𝙸𝙻𝙴𝙳: {failed} 𝙲𝙷𝙰𝚃𝚂."
             )
         except BaseException:
             failed += 1
             await lol.edit(
-                f"Userbot leaving...\n\nLeft: {left} chats.\nFailed: {failed} chats."
+                f"Userbot leaving...\n\n𝙻𝙴𝙵𝚃: {left} 𝙲𝙷𝙰𝚃𝚂.\n𝙵𝙰𝙸𝙻𝙴𝙳: {failed} 𝙲𝙷𝙰𝚃𝚂."
             )
         await asyncio.sleep(0.7)
     await client.send_message(
-        message.chat.id, f"✅ Left from: {left} chats.\n❌ Failed in: {failed} chats."
+        message.chat.id, f"✅ 𝙻𝙴𝙵𝚃 𝙵𝚁𝙾𝙼: {left} 𝙲𝙷𝙰𝚃𝚂.\n❌ 𝙵𝙰𝙸𝙻𝙴𝙳 𝙸𝙽: {failed} 𝙲𝙷𝙰𝚃𝚂."
     )
